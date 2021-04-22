899160a1329229594e546b0dfbc15b86505f0ee6
Bohdana217 <bogdanaillyk@gmail.com>
2021-04-22 20:45:29 +0300

    lab1 +calculate

---
+++ src/main/java/knu/fit/ist/ta/SimpleApp.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knu.fit.ist.ta;
+
+/**
+ *
+ * @author admin
+ */
+public class SimpleApp {
+
+    /*public static void main(String[] args) {
+    
+    MyFirstJavaClass mFJC = new MyFirstJavaClass(5); // об'єкт класу
+    
+    System.out.println(mFJC.toString());
+    
+    
+    MyFirstJavaClass.setMyStaticInt(10);
+    
+    
+    System.out.println(MyFirstJavaClass.toStringStatic());
+    
+    }*/
+
+}
