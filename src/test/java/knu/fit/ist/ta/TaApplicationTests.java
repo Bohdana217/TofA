98700f7c284a54e280cd70915ab926be90497222
Bohdana217 <bogdanaillyk@gmail.com>
2021-05-24 11:12:52 +0300

    lab3test

--- src/test/java/knu/fit/ist/ta/TaApplicationTests.java
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -1,22 +1,32 @@
 package knu.fit.ist.ta;
 
 
+import knu.fit.ist.ta.lab3.Calculation;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaApplicationTests {
+class TheoryOfAlgorithmsApplicationTests {
 
-    @Test
-    void contextLoads() {
-
-    }
-
-    @Test
-    void test1() {
-    
-    }
+	@Test
+	void contextLoads() {
+	}
+        
+        @Test
+        void testLab3() {
+            Calculation calculation = new Calculation();
+            
+            int[] x = {15, 23, 38, 12, 57};
+            int[] y = {26, 90, 73, 5, 74};
+            int[] a = {22, 19, 14, 58};
+            int[] b = {8, 82, 17, 89};
+            
+            assertEquals(x,calculation.task(y)); 
+            assertEquals(a,calculation.task(b)); 
+            
+	}
 
 }
+
 
