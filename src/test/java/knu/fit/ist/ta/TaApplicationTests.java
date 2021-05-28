51493b845bbe23fc5e9d6910e5faca821263ec13
Bohdana217 <bogdanaillyk@gmail.com>
2021-05-28 13:14:49 +0300

    deploy

--- src/test/java/knu/fit/ist/ta/TaApplicationTests.java
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -1,4 +1,4 @@
-package knu.fit.ist.ta;
+/*package knu.fit.ist.ta;
 
 
 import knu.fit.ist.ta.lab3.Calculation;
@@ -11,25 +11,25 @@ class TheoryOfAlgorithmsApplicationTests {
 
 
 
-	@Test
-	void contextLoads() {
-	}
-        
-        @Test
-        void testLab3() {
-            Calculation calculation = new Calculation();
-            
-            int[] x = {15, 23, 38, 12, 57};
-            int[] y = {26, 90, 73, 5, 74};
-            int[] a = {22, 19, 14, 58};
-            int[] b = {8, 82, 17, 89};
-            
-            assertEquals(x,calculation.task(y)); 
-            assertEquals(a,calculation.task(b)); 
-            
-	}
+@Test
+void contextLoads() {
+}
 
-    }
+@Test
+void testLab3() {
+Calculation calculation = new Calculation();
+
+int[] x = {15, 23, 38, 12, 57};
+int[] y = {26, 90, 73, 5, 74};
+int[] a = {22, 19, 14, 58};
+int[] b = {8, 82, 17, 89};
+
+assertEquals(x,calculation.task(y));
+assertEquals(a,calculation.task(b));
+
+}
+
+}*/
 
     
 
